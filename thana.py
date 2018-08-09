97a359c6f557f0170394fb4bf8d9f9c772c54927
katherineanlinc <41639286+katherineanlinc@users.noreply.github.com>
2018-08-09 09:54:59 -0400

    Update thana.py

--- thana.py
+++ thana.py
@@ -4,3 +4,10 @@ pygame.init()
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("The Chronicles of Thana")
+
+carryOn = True
+
+while carryOn:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            carryOn = False
